c7c4055ddef2e00f848b724343bc6979dc8b98ad
benak <benakkishanj@gmail.com>
2026-05-03 17:57:41 +0530

    Complete 1.11: unicafe step 6

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,33 +1,38 @@
 const StatisticLine = ({ text, value }) => {
   return (
-    <p>
-      {text}: {value}
-    </p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 };
 
 const Statistics = ({ good, neutral, bad }) => {
   return (
-    <div>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={good + neutral + bad} />
-      <StatisticLine
-        text="average"
-        value={
-          good + neutral + bad === 0 ? 0 : (good - bad) / (good + neutral + bad)
-        }
-      />
-      <StatisticLine
-        text="positive"
-        value={
-          good + neutral + bad === 0
-            ? 0
-            : (good / (good + neutral + bad)) * 100 + " %"
-        }
-      />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={good + neutral + bad} />
+        <StatisticLine
+          text="average"
+          value={
+            good + neutral + bad === 0
+              ? 0
+              : (good - bad) / (good + neutral + bad)
+          }
+        />
+        <StatisticLine
+          text="positive"
+          value={
+            good + neutral + bad === 0
+              ? 0
+              : (good / (good + neutral + bad)) * 100 + " %"
+          }
+        />
+      </tbody>
+    </table>
   );
 };
 
