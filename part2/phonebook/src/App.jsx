60a332216ed0f20d470f9760105f40122bdc4c28
benak <benakkishanj@gmail.com>
2026-05-04 20:14:13 +0530

    Complete 2.11: The Phonebook Step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Person from "./components/Person";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log(response.data)
+      setPersons(response.data)
+    })
+  }, [])
+
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -36,16 +40,12 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value);
-  };
-
   const filteredPersons =
     filter === ""
       ? persons
       : persons.filter((person) =>
-          person.name.toLowerCase().includes(filter.toLowerCase()),
-        );
+        person.name.toLowerCase().includes(filter.toLowerCase()),
+      );
 
   return (
     <div>
